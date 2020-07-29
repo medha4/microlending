050b4b283bd87a07f1902e0d811587f524087d31
Medha Gupta <medha4@gmail.com>
2020-07-29 13:38:06 +0000

    added css to user home interface

--- static/js/demo/chart-area-demo.js
+++ static/js/demo/chart-area-demo.js
@@ -1,4 +1,5 @@
 // Set new default font family and font color to mimic Bootstrap's default styling
+//data is updated by hitting control f5 or command shift r
 Chart.defaults.global.defaultFontFamily = 'Nunito', '-apple-system,system-ui,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,sans-serif';
 Chart.defaults.global.defaultFontColor = '#858796';
 
